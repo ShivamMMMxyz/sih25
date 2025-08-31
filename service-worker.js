6406a3596434c13c09923887f36090a21b49c37d
ShivamMMMxyz <kushwahashivam8037@gmail.com>
2025-08-31 13:16:39 +0530

    offline feature fixed

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,41 @@
+const CACHE_NAME = "quiz-cache-v2";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+// Install event
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("quiz-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Caching files...");
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Activate event (clear old caches)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+});
+
+// Fetch event
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
     })
   );
 });
